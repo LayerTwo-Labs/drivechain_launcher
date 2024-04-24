d55894d25afaa399459599c0a10f73621649a2a6
seriouscatsoserious <121734574+seriouscatsoserious@users.noreply.github.com>
2024-04-24 18:22:24 -0500

    Enhance Reset window apperance to match pre-existing modals

--- ui/components/settings/reset_everything_window.gd
+++ ui/components/settings/reset_everything_window.gd
@@ -11,7 +11,13 @@ func _on_reset_everything_window_close_requested() -> void:
 func _ready():
 	pass # Replace with function body.
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+func _on_center_container_gui_input(event):
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT:
+			if event.pressed:
+				queue_free()
