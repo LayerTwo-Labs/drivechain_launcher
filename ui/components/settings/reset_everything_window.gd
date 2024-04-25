440c73893731cdd89580933d307e092891c5c20e
seriouscatsoserious <121734574+seriouscatsoserious@users.noreply.github.com>
2024-04-24 19:57:54 -0500

    Add styling to Reset window and buttons

--- ui/components/settings/reset_everything_window.gd
+++ ui/components/settings/reset_everything_window.gd
@@ -1,12 +1,5 @@
 extends ColorRect
 
-func _on_reset_everything_window_close_requested() -> void:
-	var reset_window = get_node("res://ui/components/settings/reset_menu/reset_everything_window.tscn")
-	reset_window.hide()  # This hides the window but keeps it in the scene tree
-	# OR
-	reset_window.queue_free()  # This removes the window from the scene tree and frees it
-	print("Reset window closed")
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
