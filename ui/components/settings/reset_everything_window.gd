d5dcb7666e944e74b5a1dd93e0a75bc19ebcf9bb
seriouscatsoserious <121734574+seriouscatsoserious@users.noreply.github.com>
2024-04-25 10:30:18 -0500

    Add logic for reset everything to optionally save wallets

--- ui/components/settings/reset_everything_window.gd
+++ ui/components/settings/reset_everything_window.gd
@@ -14,3 +14,11 @@ func _on_center_container_gui_input(event):
 		if event.button_index == MOUSE_BUTTON_LEFT:
 			if event.pressed:
 				queue_free()
+
+
+func _on_button_pressed():
+	Appstate.reset_everything(true)
+
+
+func _on_button_2_pressed():
+	Appstate.reset_everything(false) # Replace with function body.
