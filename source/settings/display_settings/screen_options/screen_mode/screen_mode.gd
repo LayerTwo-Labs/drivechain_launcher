79ebeab09b2973d996dbbdc91e5206c6878fde91
Wamiq Ur Rehman <wamiqurrehman093@gmail.com>
2024-06-19 00:06:56 +0500

    Fix: Disabling screen resolution option button during fullscreen and switched to fullscreen instead of exclusive fullscreen

--- source/settings/display_settings/screen_options/screen_mode/screen_mode.gd
+++ source/settings/display_settings/screen_options/screen_mode/screen_mode.gd
@@ -1,13 +1,15 @@
-extends HBoxContainer
-
+class_name ScreenMode extends HBoxContainer
 
 @onready var toggle: CheckButton = $Toggle
+
+signal screen_mode_updated
 
 func _ready():
 	toggle.toggled.connect(_on_toggled)
 
 func _on_toggled(toggled_on: bool):
 	if toggled_on:
-		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_EXCLUSIVE_FULLSCREEN)
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
 	else:
 		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+	screen_mode_updated.emit(toggled_on)
