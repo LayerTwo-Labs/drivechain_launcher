61919630615f85fa00a5098e2be5b34f080064bf
Wamiq Ur Rehman <wamiqurrehman093@gmail.com>
2024-06-19 23:15:42 +0500

    Enhancement: Updates to the nodes.

--- source/nodes/node_panel/node_panel.gd
+++ source/nodes/node_panel/node_panel.gd
@@ -1,16 +1,22 @@
-class_name NodePanel extends HBoxContainer
+class_name NodePanel extends MarginContainer
 
 @export var heading: String = "NodePanel"
 @export var description: String = "Description"
+@export var disabled: bool = false
+@export_range(0.0, 1.0) var disabled_color_value = 0.85
 
-@onready var download_button: Button = $Download
-@onready var heading_label: Label = $Heading
-@onready var description_label: Label = $Description
+@onready var download_button: Button = $Container/Header/Download
+@onready var heading_label: Label = $Container/Header/Heading
+@onready var description_label: Label = $Container/Description
 
 func _ready():
-	heading_label.text = heading
+	heading_label.text = "-> " + heading
 	description_label.text = description
 	download_button.pressed.connect(_on_download_button_pressed)
+	if disabled:
+		download_button.hide()
+		heading_label.self_modulate.v = disabled_color_value
+		description_label.self_modulate.v = disabled_color_value
 
 func _on_download_button_pressed():
 	var animation_player: AnimationPlayer = download_button.get_node("AnimationPlayer")
@@ -22,4 +28,3 @@ func _on_download_button_pressed():
 		animation_player.play("running")
 	elif animation_player.current_animation == "running":
 		animation_player.play("run")
-	print(animation_player.current_animation)
