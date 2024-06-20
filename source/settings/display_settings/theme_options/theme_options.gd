cfca89b6cb23204fcb8dbaee466e47a07b11d2d1
Wamiq Ur Rehman <wamiqurrehman093@gmail.com>
2024-06-20 11:08:27 +0500

    Fix: Making the black theme default.

--- source/settings/display_settings/theme_options/theme_options.gd
+++ source/settings/display_settings/theme_options/theme_options.gd
@@ -9,9 +9,12 @@ const checked_icon = preload("res://assets/icons/CheckButton/CheckButton_Checked
 var active_option: Button
 
 func _ready():
-	active_option = options.get_node("Blue")
 	for option: Button in options.get_children():
 		option.toggled.connect(_on_option_toggled.bind(option))
+		option.icon = unchecked_icon
+		if option.button_pressed:
+			active_option = option
+			option.icon = checked_icon
 
 func _on_option_toggled(toggled_on: bool, option: Button):
 	var application = get_tree().get_first_node_in_group("application")
